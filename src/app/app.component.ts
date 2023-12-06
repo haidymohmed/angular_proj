1a9d7889d0fd9520a0faf33c2d51e0be6c7d8415
haidy.mohamed <haidy.mohamed@rosenfieldhealth.com>
2023-12-06 16:39:40 +0200

    header selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  feature = 'Recipe';
+
+  selectFeature(feature : string){
+    console.log("TEST"  + feature);
+    
+    this.feature = feature;
+  }
 }
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+
+  @Output('selectFeature') selectFeature = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSelectFeature(feature : string){
+    this.selectFeature.emit(feature);
+    console.log(feature);
+    
   }
 
 }
